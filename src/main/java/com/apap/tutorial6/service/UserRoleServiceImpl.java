5a0536ecd81615ee744ba0581af9ad4fa0455ba7
rakaprmd <raka.pramada@gmail.com>
2018-11-06 14:44:30 +0700

    latihan selesai

---
+++ src/main/java/com/apap/tutorial6/service/UserRoleServiceImpl.java
@@ -0,0 +1,27 @@
+package com.apap.tutorial6.service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.apap.tutorial6.model.UserRoleModel;
+import com.apap.tutorial6.repository.UserRoleDb;
+
+@Service
+public class UserRoleServiceImpl implements UserRoleService{
+    @Autowired
+    private UserRoleDb userDb;
+
+    public UserRoleModel addUser(UserRoleModel user) {
+        String pass = encrypt(user.getPassword());
+        user.setPassword(pass);
+        return userDb.save(user);
+    }
+
+    public String encrypt(String password) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String hashedPassword = passwordEncoder.encode(password);
+        return hashedPassword;
+    }
+
+
+}
